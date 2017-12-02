cea7714039c14385823adb709d327f20ffa4188f
KaungHtetLin <kaunghtetlin74.khl@gmail.com>
2017-12-02 12:22:58 +0630

    right drawer added

---
+++ app/src/main/java/net/kaunghtetlin/animation/AnimationApp.java
@@ -0,0 +1,10 @@
+package net.kaunghtetlin.animation;
+
+import android.app.Application;
+
+/**
+ * Created by Kaung Htet Lin on 11/28/2017.
+ */
+
+public class AnimationApp extends Application {
+}
